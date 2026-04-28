087eb3bb030ce93050a2f058df3e8f3f0dab0282
omkardyp005-rgb <omkardyp005@gmail.com>
2026-04-28 14:20:28 +0530

    Create Internal.cpp

---
+++ Internal.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Merge two halves
+void merge(int arr[], int l, int m, int r) {
+    int i = l, j = m + 1, k = 0;
+    int temp[100];   // temporary array
+
+    while (i <= m && j <= r) {
+        if (arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= m)
+        temp[k++] = arr[i++];
+
+    while (j <= r)
+        temp[k++] = arr[j++];
+
+    // Copy back to original array
+    for (i = l, k = 0; i <= r; i++, k++) {
+        arr[i] = temp[k];
+    }
+}
+
+// Merge Sort function
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int n, arr[100];
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
